07033b170424b4f1441afa78a8b2112d96b0ccb0
Jaakko Roivas <108202147+Jaqt@users.noreply.github.com>
2025-11-24 21:34:54 +0200

    vk 4, teht 4

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -191,3 +191,113 @@ class TestKauppa(unittest.TestCase):
         # varmistetaan, että metodia tilisiirto on kutsuttu
         pankki_mock.tilisiirto.assert_called_with("pekka", 42, "12345", kauppa._kaupan_tili, 5)
         # toistaiseksi ei välitetä kutsuun liittyvistä argumenteista
+
+    def test_aloita_asiointi_nollaa_hinnan(self):
+        pankki_mock = Mock()
+        viitegeneraattori_mock = Mock()
+
+        # palautetaan aina arvo 42
+        viitegeneraattori_mock.uusi.return_value = 42
+
+        varasto_mock = Mock()
+
+        # tehdään toteutus saldo-metodille
+        def varasto_saldo(tuote_id):
+            if tuote_id == 1:
+                return 10
+
+        # tehdään toteutus hae_tuote-metodille
+        def varasto_hae_tuote(tuote_id):
+            if tuote_id == 1:
+                return Tuote(1, "maito", 5)
+
+        # otetaan toteutukset käyttöön
+        varasto_mock.saldo.side_effect = varasto_saldo
+        varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
+
+        # alustetaan kauppa
+        kauppa = Kauppa(varasto_mock, pankki_mock, viitegeneraattori_mock)
+
+        # tehdään ostokset
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.tilimaksu("pekka", "12345")
+
+        # seuraava asiakas
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.tilimaksu("matti", "54321")
+
+        # varmistetaan, että metodia tilisiirto on kutsuttu
+        pankki_mock.tilisiirto.assert_called_with("matti", 42, "54321", kauppa._kaupan_tili, 5)
+        # toistaiseksi ei välitetä kutsuun liittyvistä argumenteista
+
+    def test_kauppa_pyytää_uuden_viitenumeron_jokaiselle_tapahtumalle(self):
+        pankki_mock = Mock()
+        viitegeneraattori_mock = Mock()
+
+        # palautetaan aina arvo 42
+        viitegeneraattori_mock.uusi.return_value = 42
+
+        varasto_mock = Mock()
+
+        # tehdään toteutus saldo-metodille
+        def varasto_saldo(tuote_id):
+            if tuote_id == 1:
+                return 10
+
+        # tehdään toteutus hae_tuote-metodille
+        def varasto_hae_tuote(tuote_id):
+            if tuote_id == 1:
+                return Tuote(1, "maito", 5)
+
+        # otetaan toteutukset käyttöön
+        varasto_mock.saldo.side_effect = varasto_saldo
+        varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
+        viitegeneraattori_mock.uusi.side_effect = [1, 2]
+
+        # alustetaan kauppa
+        kauppa = Kauppa(varasto_mock, pankki_mock, viitegeneraattori_mock)
+
+        # tehdään ostokset
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.tilimaksu("pekka", "12345")
+
+        # seuraava asiakas
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.tilimaksu("matti", "54321")
+
+        self.assertEqual(viitegeneraattori_mock.uusi.call_count, 2)
+        pankki_mock.tilisiirto.assert_any_call("pekka", 1, "12345", kauppa._kaupan_tili, 5)
+        pankki_mock.tilisiirto.assert_any_call("matti", 2, "54321", kauppa._kaupan_tili, 5)
+
+    def test_poista_korista_palauttaa_tuotteen_varastoon(self):
+            pankki = Mock()
+            viitteet = Mock()
+            varasto = Mock()
+
+            maito = Tuote(1, "maito", 5)
+            varasto.saldo.return_value = 1
+            varasto.hae_tuote.side_effect = [maito, maito]
+
+            k = Kauppa(varasto, pankki, viitteet)
+            k.aloita_asiointi()
+            k.lisaa_koriin(1)
+            k.poista_korista(1)
+
+            varasto.palauta_varastoon.assert_called_once_with(maito)
+
+    def test_lisaa_koriin_ei_ota_eika_hae_kun_tuote_loppu(self):
+        pankki = Mock()
+        viitteet = Mock()
+        varasto = Mock()
+
+        varasto.saldo.return_value = 0
+        k = Kauppa(varasto, pankki, viitteet)
+        k.aloita_asiointi()
+        k.lisaa_koriin(1)
+
+        varasto.hae_tuote.assert_not_called()
+        varasto.ota_varastosta.assert_not_called()
